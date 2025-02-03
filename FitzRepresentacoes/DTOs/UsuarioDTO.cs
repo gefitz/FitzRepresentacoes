208d4a5c1828b6f1e45e20529e7a3da7e528396d
Geovane <almeidafitz@gmail.com>
2025-02-03 20:03:11 -0300

    Ajustado nomes dos campos no cadastro pro usuario

--- FitzRepresentacoes/DTOs/UsuarioDTO.cs
+++ FitzRepresentacoes/DTOs/UsuarioDTO.cs
@@ -8,11 +8,13 @@ namespace FitzRepresentacoes.DTOs
         public string Nome { get; set; }
         public string Documento { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Senha")]
         public string Password { get; set; }
         public CidadeDTO Cidade { get; set; }
         public string CEP { get; set; }
         public string Rua { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name ="Data de Nascimento")]
         public DateTime dthNascimento { get; set; }
 
     }
