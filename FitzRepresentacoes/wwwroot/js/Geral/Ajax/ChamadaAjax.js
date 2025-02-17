0df5479878a3f4f69356e34fe6c339a1a9420909
Geovane <almeidafitz@gmail.com>
2025-02-17 18:47:26 -0300

    Adicionado sript do ajax no codigo e alterado a forma de exibir a modal de erro pra acompanhar o ajax, ajustado codigo de login junto

---
+++ FitzRepresentacoes/wwwroot/js/Geral/Ajax/ChamadaAjax.js
@@ -0,0 +1,17 @@
+﻿function ChamadasAjax(parametros) {
+    console.log(parametros);
+    $.ajax({
+        url: parametros.url,
+        type: "POST",
+        data: parametros.data,
+        success: function (resp) {
+            console.log(resp);
+            if (!resp.succes) {
+                console.log("Ola");
+                ShowModalError(resp.errors)
+            } else {
+                location.reload();
+            }
+        }
+    })
+}
---
+++ FitzRepresentacoes/wwwroot/js/Login/Login.js
@@ -0,0 +1,11 @@
+﻿function Authentiar() {
+    var formValue = {
+        Email: $("#Email").val(),
+        Password: $("#Password").val()
+    }
+    var parametro = {
+        url: "/Login/Login",
+        data: formValue
+    }
+    ChamadasAjax(parametro);
+}
