c74b73d201a21529c3d365867772635a8f8d3809
Geovane <almeidafitz@gmail.com>
2025-02-17 17:23:38 -0300

    Finalizado a tela de Tipo Produto

---
+++ FitzRepresentacoes/wwwroot/js/TpProdutoJs/TpProdutoModal.js
@@ -0,0 +1,42 @@
+﻿function PreencherFormulario(id, tpProduto, descricao) {
+    var formValidacao = {
+        TpProduto: { required: true },
+        Descricao: { required: true }
+    }
+    CriaValidacaoForm("#formTpProduto", formValidacao)
+    if (id != '' && tpProduto != '' && descricao != '') {
+        $("#TituloEditar").text("Editar - " + tpProduto)
+        $("#CadastroTpProduto").val(tpProduto);
+        $("#CadastroDescricao").val(descricao);
+        $("#id").val(id);
+    } else {
+        $("#TituloEditar").text("Cadastar Tipo Produto");
+        $("#CadastroTpProduto").val("");
+        $("#CadastroDescricao").val("");
+    }
+}
+
+function SalvarTipoProduto() {
+    if ($("#formTpProduto").valid()) {
+        console.log("Entro no if");
+        var tpProdutoJSO = {
+            id: $("#id").val(),
+            TpProduto: $("#CadastroTpProduto").val(),
+            Descricao: $("#CadastroDescricao").val()
+
+        }
+        $.ajax({
+            url: "/TpProduto/CadastrarTpProduto",
+            type: "POST",
+            data: tpProdutoJSO,
+            success: function (resp) {
+                if (!resp.success) {
+                ShowModalError(resp.errors)
+                }
+                    $("#tpProdutoModal").hide();
+                    location.reload();
+                    return;
+            }
+        })
+    }
+}
